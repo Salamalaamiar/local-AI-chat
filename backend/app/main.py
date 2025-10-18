2a4bff3761cbf7c8079000f7be27c5eb878661b7
salama <salamalaamiar@gmail.com>
2025-10-18 15:09:43 +0100

    add CORSMiddleare to allow requests fron frontend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from app.api import chat
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app=FastAPI(title="Local chat ai")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], #for dev only
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+
+)
 
 app.include_router(chat.router)
 @app.get("/")
